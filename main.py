649fbbb9d13433bc54a02308d9f891fce3449221
Calloway Sutton <calloway@server1.erudite.host>
2023-01-29 01:22:17 +0000

    Add the beginnings of the nodes

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 from routes.graph import graph_bp
 from routes.index import index_bp
 
+from routes.api.graph import graph_api_bp
+
+# Client facing endpoints
 app.register_blueprint(graph_bp)
 app.register_blueprint(index_bp)
+
+# API Endpoints
+app.register_blueprint(graph_api_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- routes/api/graph.py
+++ routes/api/graph.py
@@ -1,6 +1,27 @@
 from flask import Blueprint, request, jsonify, render_template
+import http.client
+
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+
+OPENAIKEY = getenv('OPENAIKEY')
+
+headers = {
+    'Authorization': f"{OPENAIKEY}",
+    'content-type': "application/json"
+    }
 
 graph_api_bp = Blueprint('graph_api', __name__)
 @graph_api_bp.route('/api/graph', methods=['GET'])
 def graph():
-    pass
+    conn = http.client.HTTPSConnection("api.openai.com")
+    payload = """ {\n  "model": "text-davinci-003",\n  "prompt": "list 10 things that I could learn while driving  and include driving in the concept below:",   \n "max_tokens": 512,\n  "temperature": 0\n}\n"""
+    conn.request("POST", "/v1/completions", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    list = data.decode("utf-8")
+
+    return list
