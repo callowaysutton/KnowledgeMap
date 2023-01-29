695eca7a74088364e0b42080f1fee6209b549496
Calloway Sutton <calloway@server1.erudite.host>
2023-01-29 00:19:41 +0000

    comments

--- routes/index.py
+++ routes/index.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template
 
+# Just serve the index.html landing page
+# SHOULD NOT have any other logic other than serving the landing page
 index_bp = Blueprint('index', __name__)
 @index_bp.route('/', methods=['GET'])
 def index():
