b2b436b5a88965458763cbc43298a9982f072e13
Calloway Sutton <calloway@server1.erudite.host>
2023-01-29 00:18:05 +0000

    added more to skeleton

--- routes/graph.py
+++ routes/graph.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template
 
+# Makes the blueprint for the /graph route 
+# Example query may be /graph?q=QUERY which returns the graph of nodes which correspond to the query
 graph_bp = Blueprint('graph', __name__)
 @graph_bp.route('/graph', methods=['GET'])
 def graph():
